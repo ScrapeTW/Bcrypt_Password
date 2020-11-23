60c94b9f91b8fdff759904b64ab4437fc43a836c
fbouazza <fbouazza.pro@gmail.com>
2020-11-23 09:00:21 +0100

    test password

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const bcrypt = require("bcryptjs")
 
 const hashPassword = async (password) => {
-    console.log("Le mot de passe de base est : " + password)
+
     const hashedPassword = await bcrypt.hash(password, 8)
-    return hashedPassword
+
+    console.log("Le mot de passe de base est : " + password)
+    console.log("Le mot de passe crypté est : " + hashedPassword)
+    const isMatch = await bcrypt.compare("UnD", hashedPassword)
+    return isMatch
+
 }
 
 hashPassword("UnDolibarr").then((result) => {
-    console.log("Le mot de passe crypté est : " + result)
+    console.log('Le résultat de la comparaison est : ' + result)
 }).catch((e) => {
     console.log(e)
 })
